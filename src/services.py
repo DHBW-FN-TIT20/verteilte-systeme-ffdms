3e9a32c3942fb7148f758e61e767ffdeb7fa7cc4
Miriam <m.astor@gmx.de>
2023-05-16 13:07:08 +0200

    call client function with parameters

--- src/services.py
+++ src/services.py
@@ -3,6 +3,6 @@ from enum import Enum
 class Services(Enum):
     SUBSCRIBE_TOPIC = 0
     UNSUBSCRIBE_TOPIC = 1
-    PUBLISH_TOPIC=2
-    LIST_TOPICS=3
-    UPDATE_TOPIC=4
+    PUBLISH_TOPIC = 2
+    LIST_TOPICS = 3
+    UPDATE_TOPIC = 4
