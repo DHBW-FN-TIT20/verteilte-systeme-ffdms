09db2628df6f86bf0d88308bdc4e031d47b28c5a
Miriam <m.astor@gmx.de>
2023-05-16 11:36:15 +0200

    arg parse

---
+++ src/client.py
@@ -0,0 +1,19 @@
+import asyncio
+import socketio
+import argparse
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        prog="Client",
+        description="Client for Publisher"
+    )
+
+    parser.add_argument("-sub", "--subscribe", nargs="+", help="list of topics to subscribe", metavar="TOPIC")
+    parser.add_argument("-p", "--publish", help="published topic", metavar="TOPIC")
+    parser.add_argument("-m", "--message", help="message to be published to topic", metavar="STRING")
+    parser.add_argument("-s", "--server", required=True, help="server address", metavar="ADDRESS:PORT")
+
+    args = parser.parse_args()
+
+    print(args)
