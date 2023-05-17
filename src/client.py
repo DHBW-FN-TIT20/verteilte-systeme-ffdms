f0e753fe403af079a9605a64ca3e45e84d35a8fe
Miriam <m.astor@gmx.de>
2023-05-17 10:47:56 +0200

    add aexit

--- src/client.py
+++ src/client.py
@@ -1,4 +1,4 @@
-import asyncio
+import atexit
 import socketio
 import argparse
 import time
@@ -6,32 +6,46 @@ import json
 import sys
 
 from transport_message import TransportMessage
-from services import Services
 
 class Client:
 
     def __init__(self, server_id) -> None:
         self.socket = socketio.Client()
         self.socket.connect(server_id)
+
         self.socket.on("PRINT_MESSAGE", self._handleResponse)
+        self.socket.on("PRINT_MESSAGE_AND_EXIT", self._handleExitResponse)
+
+        self.subscribed_topics = []
 
     def subscribe(self, topics):
-        if not isinstance(topics, list):
-            topics = [topics]
 
-        for t in topics:
-            tMessage = TransportMessage(timestamp=time.time(), topic=t)
+        self.subscribed_topics = topics
+        # string to list
+        if not isinstance(topics, list):
+            self.subscribed_topics = [topics]
+
+        print(f"======= SUBSCRIBED TO {', '.join(self.subscribed_topics)} =======\n")
+        for topic in self.subscribed_topics:
+            tMessage = TransportMessage(timestamp=time.time(), topic=topic)
             self.socket.emit("SUBSCRIBE_TOPIC", tMessage.json())
 
-    def unsubscribe(self, topics):
-        if not isinstance(topics, list):
-            topics = [topics]
+    def unsubscibe(self):
 
-        for t in topics:
-            tMessage = TransportMessage(timestamp=time.time(), topic=t)
-            self.socket.emit("UNSUBSCRIBE_TOPIC", tMessage.json())
+        print("exxxiiittt")
+
+        # Unsubscribe        
+        # print(f"======= UNSUBSCRIBED FROM {', '.join(self.subscribed_topics)} =======")
+        #for topic in self.subscribed_topics:
+        #    tMessage = TransportMessage(timestamp=time.time(), topic=topic)
+        #    self.socket.emit("UNSUBSCRIBE_TOPIC", tMessage.json())
+        
+        # Disconnect
+        self.socket.disconnect()
 
     def publish(self, topic, message):
+        print(f"======= PUBLISH MESSAGE =======")
+        print(f"Message: {message}")
         tMessage = TransportMessage(timestamp=time.time(), topic=topic, payload=message)
         self.socket.emit("PUBLISH_TOPIC", tMessage.json())
 
@@ -42,11 +56,18 @@ class Client:
     def getTopicStatus(self, topic):
         tMessage = TransportMessage(timestamp=time.time(), topic=topic)
         self.socket.emit("GET_TOPIC_STATUS", tMessage.json())
-
-    # @socket.on("PRINT_MESSAGE")
+    
     def _handleResponse(self, response):
-        print(f"SERVER RESPONSE: {self, response}")
-
+        response_dict = json.loads(response)
+        print(f"{response_dict['payload']}")
+    
+    def _handleExitResponse(self, response):
+        self._handleResponse(response)
+        
+        # Exit
+        self.socket.disconnect()
+        sys.exit(0)
+        
 
 if __name__ == "__main__":
 
@@ -59,7 +80,7 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--publish", help="published topic", metavar="TOPIC")
     parser.add_argument("-m", "--message", help="message to be published to topic", metavar="STRING")
     parser.add_argument("-l", "--list", action="store_true", help="get all list topics from server")
-    parser.add_argument("-st", "--status", action="store_true", help="get topic status from server")
+    parser.add_argument("-st", "--status", help="get topic status from server")
     parser.add_argument("-s", "--server", required=True, help="server address", metavar="ADDRESS:PORT")
 
     args = parser.parse_args()
@@ -68,23 +89,19 @@ if __name__ == "__main__":
     cli = Client(args.server)
 
     if args.subscribe:
-        try:
-            cli.subscribe(args.subscribe)
-        except KeyboardInterrupt: # Catch Ctrl+C TODO: use exit handler
-            cli.unsubscribe(args.subscribe)
-            sys.exit()
+        cli.subscribe(args.subscribe)
+        atexit.register(cli.unsubscibe)
 
     elif args.publish and args.message:
-        cli.publish(args.publish, args.subscribe)
+        cli.publish(args.publish, args.message)
 
     elif args.list:
-        cli.listTopics(args.list)
+        cli.listTopics()
     
     elif args.status:
-        cli.listTopics(args.status)
+        cli.getTopicStatus(args.status)
 
     else:
         print("No action, please check your parameters")
     
-    sys.exit()
 
